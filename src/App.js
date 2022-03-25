d1da9043b34f38d2426453033e62a66f8327f903
Chad E <chad1020@gmail.com>
2022-03-24 23:12:51 -0500

    base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import  MediaCard from "./components/MediaCard/";
+import MenuBar from  "./components/MenuBar/";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <MenuBar/>
+    <Box container spacing={0} direction="column" alignItems="center" justifyContent="center" style={{ minHeight: '70vh', maxWidth:'150vh' }}>
+    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+      <Grid item xs={3}>
+        <MediaCard/>
+      </Grid>
+      <Grid item xs={3}>
+        <MediaCard/>
+      </Grid>
+      <Grid item xs={3}>
+        <MediaCard/>
+      </Grid>
+      <Grid item xs={3}>
+        <MediaCard/>
+      </Grid>
+    </Grid>
+  </Box>
     </div>
   );
 }
